c20fec7b8b090aa5146690f3a4205c6583396e9d
ZigzagTamizhann <114802656+ZigzagTamizhann@users.noreply.github.com>
2026-01-11 12:50:34 +0530

    Updated At 2026-01-11 12:50:34

--- Arm Car/Arm_car/include/pins.h
+++ Arm Car/Arm_car/include/pins.h
@@ -4,11 +4,11 @@
 #include <Arduino.h>
 
 // Motor A (Left)
-const int motorAPin1 = 26; 
-const int motorAPin2 = 27; 
+const int motorAPin1 = 15; 
+const int motorAPin2 = 2; 
 
 // Motor B (Right)
-const int motorBPin1 = 14; 
+const int motorBPin1 = 13; 
 const int motorBPin2 = 12; 
 
 #endif
